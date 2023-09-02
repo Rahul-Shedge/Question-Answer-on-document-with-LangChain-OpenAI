7d3c268c62ca6b53276d7a272e1712d0a9739343
Rahul-Shedge <rahulshedge555@outlook.com>
2023-09-02 19:12:33 +0530

    llm processing code added

---
+++ src/llms.py
@@ -0,0 +1,29 @@
+import tiktoken
+import streamlit as st
+
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
+   
+# 
+def ask_and_get(db,question,k=3):
+    retriever = db.as_retriever(search_type='similarity', search_kwargs={'k': k})
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.1)
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+    answer = chain.run(question)
+    return answer
+
+def calculate_embedding_cost(text):
+    encoding = tiktoken.encoding_for_model("text-embedding-ada-002")
+    total_tokens = sum([len(encoding.encode(page.page_content)) for page in text])
+    return total_tokens,total_tokens/1000*0.0004
+
+
+# clear the chat history from streamlit session state
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
